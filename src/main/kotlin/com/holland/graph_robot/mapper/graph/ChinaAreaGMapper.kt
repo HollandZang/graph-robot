db8883b9212e55919ca01dd479be44fe6866910c
ZhangHN <zhn.pop@gmail.com>
2023-06-19 18:00:33 +0800

    init

--- src/main/kotlin/com/holland/graph_robot/mapper/graph/ChinaAreaGMapper.kt
+++ src/main/kotlin/com/holland/graph_robot/mapper/graph/ChinaAreaGMapper.kt
@@ -2,6 +2,7 @@ package com.holland.graph_robot.mapper.graph
 
 import com.baomidou.dynamic.datasource.annotation.DS
 import com.holland.graph_robot.domain.ChinaArea
+import org.apache.ibatis.annotations.Delete
 import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Select
@@ -18,7 +19,7 @@ interface ChinaAreaGMapper {
     @Insert("MATCH (a:ChinaArea),(b:ChinaArea) where a.id=#{nodeA.id} and b.id=#{nodeB.id} create (a)-[r:\${relationship}]->(b) create (b)-[_r:\${relationship}]->(a)")
     fun createBothRelationship(relationship: String, nodeA: ChinaArea, nodeB: ChinaArea)
 
-    @Insert("MATCH p=()-[r:\${relationship}]->() DELETE r")
+    @Delete("MATCH p=()-[r:\${relationship}]->() DELETE r")
     fun delRelationship(relationship: String)
 
     @Select("MATCH p=(a)-[r:adjoin]->(b) where a.id=#{id} return b.id as id, b.pid as pid, b.name as name,b.fullName as fullName, b.extName as extName")
